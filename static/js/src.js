055b36547e21b4781f15901e7b7d8327d4359d94
Nicholas Fleck <nicholasfleck@gmail.com>
2015-06-27 01:35:27 -0500

    Simplify src.js

--- static/js/src.js
+++ static/js/src.js
@@ -1,17 +1,12 @@
-function escapeHTML(string)
-{
-    var pre = document.createElement('pre');
-    var text = document.createTextNode( string );
-    pre.appendChild(text);
-    return pre.innerHTML;
-}
-
 $("code[data-src]").each(function() {
   var elem = $(this);
   $.ajax({
     url: elem.attr("data-src"),
     success: function(data) {
-      elem.html(escapeHTML(data));
+      var pre = document.createElement('pre');
+      var text = document.createTextNode( string );
+      pre.appendChild(text);
+      elem.html(pre.innerHTML);
     },
     async: false
   });
