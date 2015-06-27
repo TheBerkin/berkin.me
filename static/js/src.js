6126e0b6a80c4235d9ef1d037ed17127b5bedbb9
Nicholas Fleck <nicholasfleck@gmail.com>
2015-06-27 01:36:10 -0500

    It might be nice if it worked, too

--- static/js/src.js
+++ static/js/src.js
@@ -4,7 +4,7 @@ $("code[data-src]").each(function() {
     url: elem.attr("data-src"),
     success: function(data) {
       var pre = document.createElement('pre');
-      var text = document.createTextNode( string );
+      var text = document.createTextNode(data);
       pre.appendChild(text);
       elem.html(pre.innerHTML);
     },
