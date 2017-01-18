31fb221acdd8a7be39b693883a76166e64cc887f
Nicholas Fleck <nicholasfleck@gmail.com>
2017-01-18 14:10:58 -0600

    Update some stuff

---
+++ static/txt/api-example.cs
@@ -0,0 +1,3 @@
+var rant = new RantEngine();
+rant.LoadPackage("Rantionary");
+Console.WriteLine(rant.Do(@"The <adj> <adj> <noun-animal> <verb.s-walk> over the <adj> <noun-animal>."));
--- static/txt/api_example.cs
+++
@@ -1,4 +0,0 @@
-var rant = new RantEngine();
-rant.LoadPackage("Rantionary");
-var output = rant.Do(@"The <adj> <adj> <noun-animal> <verb.s-walk> over the <adj> <noun-animal>.");
-Console.WriteLine(output);
